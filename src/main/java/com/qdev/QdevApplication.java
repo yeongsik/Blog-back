af342f41ac2fabe34caced36b52a173ff07f3c18
kwon-yeongsik <youngsik0266@gmail.com>
2023-10-19 22:39:43 +0900

    first commit

---
+++ src/main/java/com/qdev/QdevApplication.java
@@ -0,0 +1,13 @@
+package com.qdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QdevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QdevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qdev/QdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QdevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
