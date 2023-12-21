99cba821391dbdc6252c3510f2c6b6d19842f84e
ys <youngsik0266@gmail.com>
2023-12-21 11:30:56 +0900

    feat Post 테스트 코드 추가

---
+++ src/test/java/com/qdev/domain/post/controller/PostControllerTest.java
@@ -0,0 +1,22 @@
+package com.qdev.domain.post.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureWebMvc
+@SpringBootTest
+class PostControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("테스트")
+    void test() {
+
+    }
+}
---
+++ src/test/java/com/qdev/domain/post/service/PostServiceTest.java
@@ -0,0 +1,21 @@
+package com.qdev.domain.post.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+
+
+
+    @Test
+    @DisplayName("")
+    void test1() {
+
+    }
+
+
+}
